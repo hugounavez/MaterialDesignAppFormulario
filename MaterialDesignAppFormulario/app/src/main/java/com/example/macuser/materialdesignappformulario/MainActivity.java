1456baba4c7143133e6870ff84696cd8554073cd
Hugo Reyes <hugo.reyesamell@gmail.com>
2017-11-18 13:14:41 -0400

    Adding second activity

--- MaterialDesignAppFormulario/app/src/main/java/com/example/macuser/materialdesignappformulario/MainActivity.java
+++ MaterialDesignAppFormulario/app/src/main/java/com/example/macuser/materialdesignappformulario/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.macuser.materialdesignappformulario;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // Global variables
+    TextInputLayout et_fullname;
+    Button btn_next;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_next = (Button) findViewById(R.id.btn_next);
+
     }
 }
