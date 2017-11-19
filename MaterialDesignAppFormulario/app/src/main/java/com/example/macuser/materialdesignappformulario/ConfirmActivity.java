1cb6518b0de446bae7a6f5afc7aa7569b7e86a6c
Hugo Reyes <hugo.reyesamell@gmail.com>
2017-11-19 07:36:22 -0400

    Adding activities optimization

--- MaterialDesignAppFormulario/app/src/main/java/com/example/macuser/materialdesignappformulario/ConfirmActivity.java
+++ MaterialDesignAppFormulario/app/src/main/java/com/example/macuser/materialdesignappformulario/ConfirmActivity.java
@@ -1,24 +1,100 @@
 package com.example.macuser.materialdesignappformulario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.macuser.materialdesignappformulario.Model.User;
 
+import java.io.InterruptedIOException;
+
 public class ConfirmActivity extends AppCompatActivity {
+
+    // Global variables
+    // UI elements
+    TextView    fullname;
+    TextView    birthDate;
+    TextView    phone;
+    TextView    email;
+    TextView    description;
+    Button      btn;
+    // Model data
+    User        user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
 
+        // Bind UI elements with layout and set button
+        this.initializeViews();
+        // Get object passed via intent
+        this.user = this.getObjectFromBundle();
+
+        this.assignValues();
+    }
+
+    User getObjectFromBundle(){
+        // Get the extras from intent
         Bundle bundle = getIntent().getExtras();
+        // Parse the string to a User object
         String objAsJson = bundle.getString("user");
+        return User.fromJson(objAsJson);
+    }
 
-        User user = User.fromJson(objAsJson);
+    public void initializeViews(){
+        this.fullname   = (TextView) findViewById(R.id.tv_confirm_name);
+        this.birthDate  = (TextView) findViewById(R.id.tv_confirm_date);
+        this.phone      = (TextView) findViewById(R.id.tv_confirm_phone);
+        this.email      = (TextView) findViewById(R.id.tv_confirm_email);
+        this.description = (TextView) findViewById(R.id.tv_confirm_description);
+        this.btn        = (Button) findViewById(R.id.btn_edit_data);
 
-        System.out.println(user.getFullname());
+        this.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Come back to main activity
+                openMainActivity();
+            }
+        });
+    }
+
+    void assignValues(){
+        // Set UI elements with user information
+        this.fullname.setText(this.user.getFullname());
+        this.birthDate.setText(this.user.getBirthDate());
+        this.phone.setText(this.user.getPhone());
+        this.email.setText(this.user.getEmail());
+        this.description.setText(this.user.getContactDescription());
 
     }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        // Catching the back event press button
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            this.openMainActivity();
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    void openMainActivity(){
+        // This method open MainActivity
+        Intent intent = new Intent(ConfirmActivity.this, MainActivity.class);
+        String userAsJson = this.user.toJson();
+        intent.putExtra("user", userAsJson);
+        startActivity(intent);
+        finish();
+
+    }
+
+
 }
--- MaterialDesignAppFormulario/app/src/main/java/com/example/macuser/materialdesignappformulario/MainActivity.java
+++ MaterialDesignAppFormulario/app/src/main/java/com/example/macuser/materialdesignappformulario/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +15,9 @@ import android.widget.DatePicker;
 import android.widget.Toast;
 
 import com.example.macuser.materialdesignappformulario.Model.User;
-import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,13 +39,15 @@ public class MainActivity extends AppCompatActivity {
        this.initializeViews();
 
 
-
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            // Parse the string to a User object
+            String objAsJson = bundle.getString("user");
+            this.assignValues(User.fromJson(objAsJson));
+        }
 
 
     }
-
-
-
 
     public void initializeViews(){
         this.et_fullname = (TextInputEditText) findViewById(R.id.et_fullname);
@@ -50,9 +55,6 @@ public class MainActivity extends AppCompatActivity {
         this.et_email = (TextInputEditText) findViewById(R.id.et_email);
         this.et_contact = (TextInputEditText) findViewById(R.id.et_contact);
         this.datePicker = (DatePicker) findViewById(R.id.datePicker);
-
-
-
 
         btn = (Button) findViewById(R.id.btn_next);
 
@@ -64,19 +66,59 @@ public class MainActivity extends AppCompatActivity {
                 String phone = et_phone.getText().toString();
                 String email = et_email.getText().toString();
                 String contact = et_contact.getText().toString();
-                
-                User user = new User(fullname, "Fecha de nacimiento", phone, email, contact);
+                String datepickerString     = getStringDateFromDatePicker(datePicker);
+
+                if (fullname.trim().isEmpty() || phone.trim().isEmpty() ||
+                        email.trim().isEmpty() || contact.trim().isEmpty()
+                        || datepickerString.trim().isEmpty()){
+
+                    Snackbar.make(v, "Complete the requested information", Snackbar.LENGTH_LONG).show();
+                    return;
+                }
+
+                User user = new User(fullname, datepickerString, phone, email, contact);
                 String objAsJson = user.toJson();
 
                 Intent intent = new Intent(MainActivity.this, ConfirmActivity.class);
                 intent.putExtra("user", objAsJson);
                 startActivity(intent);
+                finish();
 
 
             }
         });
    }
 
+
+    void assignValues(User user){
+        // Set UI elements with user information
+        this.et_fullname.setText(user.getFullname());
+        this.et_email.setText(user.getEmail());
+        this.et_phone.setText(user.getPhone());
+        this.et_contact.setText(user.getContactDescription());
+
+
+
+    }
+
+   public static String getStringDateFromDatePicker (DatePicker datePicker){
+       int day = datePicker.getDayOfMonth();
+       int month = datePicker.getMonth();
+       int year =  datePicker.getYear();
+
+       return String.valueOf(day) + "/" + String.valueOf(month) + "/" + String.valueOf(year);
+   }
+
+//    public static java.util.Date getDateFromDatePicker(DatePicker datePicker){
+//        int day = datePicker.getDayOfMonth();
+//        int month = datePicker.getMonth();
+//        int year =  datePicker.getYear();
+//
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(year, month, day);
+//
+//        return calendar.getTime();
+//    }
 
 
 }
